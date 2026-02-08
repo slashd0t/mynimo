7e869620cc97b3a7d73c91df3f7327a10209a187
sudogem <sudogem@gmail.com>
2026-02-08 21:46:21 +0800

    Update forms/onboarding/OnboardingForm.tsx

--- src/components/forms/onboarding/OnboardingForm.tsx
+++ src/components/forms/onboarding/OnboardingForm.tsx
@@ -36,9 +36,7 @@ const OnboardingForm = () => {
     <>
       <div className="flex items-center gap-4 mb-10">
         <Image src={logo} alt="logo" width={50} height={50} />
-        <h1 className="text-4xl font-bold">
-          Hire<span className="text-primary">ek</span>
-        </h1>
+        <h1 className="text-4xl font-bold">Trabaho</h1>
       </div>
 
       <Card className="shadow-none sm:min-w-lg">
