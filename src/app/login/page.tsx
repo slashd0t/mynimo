ac55fed794275e67d0cade270c89abff1ae0e080
sudogem <sudogem@gmail.com>
2026-02-08 21:25:51 +0800

    Update src/app/favicon.ico

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,9 +10,7 @@ const page = () => {
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link href="/" className="flex items-center gap-2 self-center">
           <Image src={logo} alt="logo" width={40} height={40} className="size-10" />
-          <h1 className="text-2xl font-bold">
-            Hire<span className="text-primary">ek</span>
-          </h1>
+          <h1 className="text-2xl font-bold">Trabaho</h1>
         </Link>
 
         {/* Login Form */}
